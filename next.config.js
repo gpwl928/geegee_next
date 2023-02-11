449f17ca8046286dc2b1d9418eb258b2100d5386
노혜지 <gpwl928@gmail.com>
2023-02-11 20:42:28 +0900

    feat: 페이지 리다이렉트 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [{
+      source: '/',
+      destination: '/gamestore',
+      permanent: true
+    }]
+  }
 }
 
 module.exports = nextConfig
