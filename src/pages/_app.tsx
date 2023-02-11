fd2b680542d634ca5a6598ad1488c2890fb38b86
노혜지 <gpwl928@gmail.com>
2023-02-11 20:40:54 +0900

    fix: 헤더 푸터 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import { InitTheme } from '../styles/Theme';
 import ResetCss from '../styles/ResetCss';
-import Header from 'src/components/GGHeader';
-import Footer from 'src/components/GGFooter';
+import Header from '@/components/layout/GGHeader';
+import Footer from '@/components/layout/GGFooter';
 
 const App = ({ Component, pageProps }: AppProps) => {
   
@@ -20,9 +20,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       </Head>
       <ThemeProvider theme={InitTheme}>
         <ResetCss theme={InitTheme()} />
-        <Header />
         <Component {...pageProps} />
-        <Footer />
       </ThemeProvider>
     </>
   )
